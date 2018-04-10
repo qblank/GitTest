8734ef68ff47d8d14dce9e44a8733a0a06ef3f97
evan_qb <evan_qb@DESKTOP-A2H4KJL>
2018-04-10 17:10:49 +0800

    修改错误

--- src/cn/qblank/test/GitTest.java
+++ src/cn/qblank/test/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
 		System.out.println("evan_qb");
 		System.out.println("ej");
 		System.out.println("evan_qb第三次提交");
-		System.out.println("ej第三次提交")
+		System.out.println("ej第三次提交");
 	}
 }
